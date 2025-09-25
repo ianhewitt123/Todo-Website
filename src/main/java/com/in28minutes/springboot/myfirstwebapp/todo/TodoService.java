82a5c4af75fd31a9a92f63c9b34d4cefa9e3b2ea
ianhewitt123 <ianhewitt5611@gmail.com>
2025-09-25 19:30:23 -0400

    Initial commit from Eclipse

---
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
@@ -0,0 +1,47 @@
+package com.in28minutes.springboot.myfirstwebapp.todo;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.validation.Valid;
+
+@Service
+public class TodoService {
+	private static List<Todo> todos = new ArrayList<>();
+	private static int todosCount = 0;
+	static {
+		todos.add(new Todo(++todosCount,"in28minutes","Get AWS Certified 1",LocalDate.now().plusYears(1),false));
+		todos.add(new Todo(++todosCount,"in28minutes","Learn DevOps 1",LocalDate.now().plusYears(2),false));
+		todos.add(new Todo(++todosCount,"in28minutes","Learn Full Stack Development 1",LocalDate.now().plusYears(3),false));
+	}
+	
+	public List<Todo> findByUsername(String username){
+		Predicate<? super Todo> predicate = todo -> todo.getUsername().equalsIgnoreCase(username);
+		return todos.stream().filter(predicate).toList();
+	}
+	
+	public void addTodo(String username, String Description, LocalDate targetDate, boolean done) {
+		todos.add(new Todo(++todosCount,username,Description,targetDate,done));
+	}
+	
+	public void deleteTodo(int id) {
+		//todo.getId() == id then remove
+		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+		todos.removeIf(predicate);
+	}
+
+	public Todo findById(int id) {
+		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+		Todo todo = todos.stream().filter(predicate).findFirst().get();
+		return todo;
+	}
+
+	public void updateTodo(@Valid Todo todo) {
+		deleteTodo(todo.getId());
+		todos.add(todo);
+	}
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001, 'in28minutes', 'Get AWS Certfieid', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002, 'in28minutes', 'Get AZURE Certfieid', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003, 'in28minutes', 'Get JAVA Certfieid', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004, 'in28minutes', 'Get Dev0ps Certfieid', CURRENT_DATE(), false);
